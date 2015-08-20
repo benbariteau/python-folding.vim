fcb6f5d27ef37ad27e09b8aca601f76ad419fbf1
Ben Bariteau <firba1@gmail.com>
2015-08-20 10:28:11 -0700

    Adding test file.

---
+++ test.py
@@ -0,0 +1,36 @@
+from butt import poop
+
+import stuff
+
+from foo import bar
+
+
+class Baz(object):
+    def thing(self):
+        pass
+
+    @redecorate
+    @decorate
+    def thing2(self):
+        def thing3():
+            pass
+    
+    def things(
+        butt,
+        fart,
+    ):
+        pass
+
+    def really_long_method_name(
+        really_long_posarg_name,
+        extra_super_long_kwarg_name=THIS_IS_PROBABLY_MORE_THAN_EIGHTY_CHARACTERS,
+    ):
+        pass
+
+
+@decorum(
+    a_lot=True,
+)
+@fart
+def butt(fart):
+    pass
